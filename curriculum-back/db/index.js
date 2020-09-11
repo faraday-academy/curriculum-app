be14d8578980ae1e78c67bdf544f0fb3759feb27
gwenf <gwenfaraday@pm.me>
2020-09-11 00:09:03 -0400

    update db connection

--- curriculum-back/db/index.js
+++ curriculum-back/db/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV === 'production') {
-  const username = process.env.MONGO_INITDB_ROOT_USERNAME
-  const password = process.env.MONGO_INITDB_ROOT_PASSWORD
-  mongoose.connect(`mongodb://${username}:${password}@mongo/curriculumapp`, {
+  mongoose.connect(`mongodb://mongo/curriculumapp`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
