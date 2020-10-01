af0b1b120c6c073a9f7a216a2ed22c9b83167245
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-10-01 17:14:56 +0100

    Add Title and Logo Home Route

--- curriculum-front/src/components/TopNav.vue
+++ curriculum-front/src/components/TopNav.vue
@@ -2,43 +2,35 @@
   <v-toolbar color="info">
     <v-toolbar-title>
       <v-icon left>mdi-school</v-icon>
-      <b>Curricula App</b>
+      <b
+        ><router-link
+          to="/"
+          style="text-decoration: none; color: black; outline: none"
+          >Curricula App</router-link
+        ></b
+      >
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
 
-    <v-toolbar-items
-      v-if="$vuetify.breakpoint.xsOnly"
-    >
-      <v-btn
-        @click="openDrawer()"
-      >
-        Menu
-      </v-btn>
+    <v-toolbar-items v-if="$vuetify.breakpoint.xsOnly">
+      <v-btn @click="openDrawer()"> Menu </v-btn>
     </v-toolbar-items>
 
-    <v-toolbar-items
-      v-else
-    >
-      <v-btn to="/" exact text>
-        Home
-      </v-btn>
-      <v-btn to="/curricula" exact text>
-        View All
-      </v-btn>
-      <v-btn v-if="user.token" to="/curricula/create" text>
-        Create
-      </v-btn>
-      <v-btn v-if="!user.token" to="/login" color="secondary" class="black--text">
+    <v-toolbar-items v-else>
+      <v-btn to="/" exact text> Home </v-btn>
+      <v-btn to="/curricula" exact text> View All </v-btn>
+      <v-btn v-if="user.token" to="/curricula/create" text> Create </v-btn>
+      <v-btn
+        v-if="!user.token"
+        to="/login"
+        color="secondary"
+        class="black--text"
+      >
         Log In
       </v-btn>
 
-      <v-menu
-        v-else
-        left
-        bottom
-        offset-y
-      >
+      <v-menu v-else left bottom offset-y>
         <template v-slot:activator="{ on }">
           <v-btn v-on="on" color="secondary" class="black--text">
             <v-icon left>mdi-account-outline</v-icon>
@@ -47,45 +39,39 @@
         </template>
 
         <v-list>
-          <v-list-item
-            to="/settings"
-          >
+          <v-list-item to="/settings">
             <v-list-item-title>Settings</v-list-item-title>
           </v-list-item>
-          <v-list-item
-            @click="logout"
-          >
+          <v-list-item @click="logout">
             <v-list-item-title>Logout</v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
     </v-toolbar-items>
-
   </v-toolbar>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
   props: {
     openDrawer: {
       type: Function,
-      required: true
-    }
+      required: true,
+    },
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
   computed: {
-    ...mapState('auth', ['user'])
+    ...mapState("auth", ["user"]),
   },
   methods: {
-    ...mapActions('auth', ['logUserOut']),
-    logout () {
-      this.logUserOut()
-    }
-  }
-}
+    ...mapActions("auth", ["logUserOut"]),
+    logout() {
+      this.logUserOut();
+    },
+  },
+};
 </script>
