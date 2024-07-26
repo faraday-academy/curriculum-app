d40b072fc021b7e94b6408ef4f31d24ec0e43548
Gwendolyn Faraday <gwenfaraday@pm.me>
2024-07-26 08:48:05 -0400

    add auth components

---
+++ curriculum-front/src/views/auth/ForgotPassword.vue
@@ -0,0 +1,44 @@
+<template>
+  <AuthTemplate>
+    <template #title>
+      Forgot Password
+    </template>
+    <template #form>
+      <v-text-field
+        label="Email"
+        v-model="email"
+      />
+    </template>
+    <template #actions>
+      <v-btn @click="submit" color="primary">Submit</v-btn>
+    </template>
+    <template #link>
+      <p class="pa-2">Don't have an account? <router-link :to="{name: 'register'}">Register here</router-link></p>
+    </template>
+  </AuthTemplate>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import AuthTemplate from './AuthTemplate'
+
+export default {
+  name: 'forgot-password',
+  components: {
+    AuthTemplate
+  },
+  data() {
+    return {
+      email: '',
+    }
+  },
+  methods: {
+    ...mapActions('auth', ['']),
+    submit() {
+      const payload = {
+        email: this.email
+      }
+    }
+  }
+}
+</script>
---
+++ curriculum-front/src/views/auth/ResetPassword.vue
@@ -0,0 +1,55 @@
+<template>
+  <AuthTemplate>
+    <template #title>
+      Reset Password
+    </template>
+    <template #form>
+      <v-text-field
+        label="Code"
+        v-model="code"
+      />
+      <v-text-field
+        label="New Password"
+        v-model="password"
+      />
+      <v-text-field
+        label="Confirm Password"
+        v-model="confirmPassword"
+      />
+    </template>
+    <template #actions>
+      <v-btn @click="submit" color="primary">Submit</v-btn>
+    </template>
+    <template #link>
+      <p class="pa-2">Don't have an account? <router-link :to="{name: 'register'}">Register here</router-link></p>
+    </template>
+  </AuthTemplate>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import AuthTemplate from './AuthTemplate'
+
+export default {
+  name: 'reset-password',
+  components: {
+    AuthTemplate
+  },
+  data() {
+    return {
+      code: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  methods: {
+    ...mapActions('auth', ['login']),
+    submit() {
+      const payload = {
+        password: this.password
+      }
+      this.login(payload)
+    }
+  }
+}
+</script>
